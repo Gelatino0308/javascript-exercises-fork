42f59297660ffadd3a10921cf525d07e69560ad0
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2024-08-27 15:25:56 +0800

    test: finish js exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, numTimes) {
+    if (numTimes >= 0) {
+        let repeated = '';
+        for (let i = 0; i < numTimes; i++) {
+            repeated += str;
+        }
+        return repeated;
+    }
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
