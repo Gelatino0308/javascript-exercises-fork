e449bd681b46f12523dc85bba3a27c0750ccd465
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2024-08-27 17:55:45 +0800

    test: finish js exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+
+    const newArr = arr.filter(item => {
+        let notTarget = true;
+        for (const target of theArgs) {
+            if (item === target) {
+                notTarget = false;
+            }
+        }
+        if (notTarget === true) return item;
+    });
+
+    return newArr;
 };
 
 // Do not edit below this line
