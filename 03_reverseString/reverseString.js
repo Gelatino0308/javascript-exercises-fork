74df2d1bab462ae6db65f787b01fbb2a7adee80f
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2024-08-27 15:35:52 +0800

    test: finish js exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let charArr = str.split("");
+    let strReverse = '';
 
+    for (let i = charArr.length - 1; i >= 0; i--) { 
+        strReverse += charArr[i];
+    }
+
+    return strReverse;
 };
 
 // Do not edit below this line
