f7e28827541435f1f7667e17c444a0cdbdfac668
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2024-08-27 18:57:41 +0800

    test: finish js exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let temp = (degree - 32) * 5 / 9;
+  temp = Math.round(temp * 10) / 10;
+  return temp;  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let temp = degree * 9 / 5 + 32;
+  temp = Math.round(temp * 10) / 10;
+  return temp;
 };
 
 // Do not edit below this line
