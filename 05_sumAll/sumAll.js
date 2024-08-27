7360efc98f65ac354276827227d6e89fd9e2eb80
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2024-08-27 18:26:17 +0800

    test: finish js exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
 
+    if (firstNum < 0 || lastNum < 0 || !Number.isInteger(firstNum) || !Number.isInteger(lastNum)) {
+        return 'ERROR';
+    }
+    else {
+        let sum = 0;
+        if (firstNum > lastNum) {
+            let temp = firstNum;
+            firstNum = lastNum;
+            lastNum = temp;
+        }
+        while (firstNum <= lastNum) {
+            sum += firstNum;
+            firstNum++;
+        }
+
+        return sum;
+    }
 };
 
 // Do not edit below this line
