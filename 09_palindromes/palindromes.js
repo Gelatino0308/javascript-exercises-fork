d683a0d278db74f332565d2439e5eb0c1b280092
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2024-09-14 19:19:58 +0800

    test: finish js exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const str = string.toLowerCase();
+    const charArr = str.split("");
+    const checkedArr = charArr.filter((char) => alphanumeric.includes(char));
+    const checkedStr = checkedArr.join("");
+    const reversedArr = checkedArr.reverse();
+    const reversedStr = reversedArr.join("");
+    
 
+    if (reversedStr === checkedStr) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
