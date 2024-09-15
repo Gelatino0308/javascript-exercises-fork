36238cefc9a502166ed375b4917ed8128a48b1e6
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2024-09-15 21:07:10 +0800

    test: finish js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
 
+    return peopleArr.reduce((currOldest, currPerson) => {
+
+        let currPersonDeath = currPerson.yearOfDeath;
+        let currOldestDeath = currOldest.yearOfDeath;
+
+        const currYear = new Date().getFullYear();
+
+        if (!currPersonDeath) {
+            currPersonDeath = currYear;
+        }
+
+        if (!currOldestDeath) {
+            currOldestDeath = currYear;
+        }
+
+        const currPersonAge = currPersonDeath - currPerson.yearOfBirth;
+        const currOldestAge = currOldestDeath - currOldest.yearOfBirth;
+
+        return currPersonAge < currOldestAge ? currOldest : currPerson;
+    });
 };
 
 // Do not edit below this line
