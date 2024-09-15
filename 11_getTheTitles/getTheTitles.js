4154c713497625dec5218d3b4a5b71ab206d6790
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2024-09-15 20:01:59 +0800

    test: finish js exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArr) {
+    const titles = [];
+    for (book of bookArr) {
+        titles.push(book.title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
